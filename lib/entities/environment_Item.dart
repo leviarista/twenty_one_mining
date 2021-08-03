5c38e6d8f38e7d3dd95bd858dccbab618f919248
Leví Arista <levi.arista@gmail.com>
2021-08-03 17:50:21 -0500

    Bedroom Environment

---
+++ lib/entities/environment_Item.dart
@@ -0,0 +1,19 @@
+class EnvironmentItem {
+  final int index;
+  final String image;
+  final String mode;
+  final double? top;
+  final double? right;
+  final double? bottom;
+  final double? left;
+
+  EnvironmentItem(
+    this.index,
+    this.image,
+    this.mode,
+    this.top,
+    this.right,
+    this.bottom,
+    this.left,
+  );
+}
